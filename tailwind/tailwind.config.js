8747268c390aa51d8774bc7ce0cf64f4fc9f9a50
harlin <harlin@localhost>
2025-09-04 06:34:56 -0600

    Issue #9 - Add copy button

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
   content: [
     "../pub/apps/**/templates/**/*.{html,py,js}",
     "../pub/**/templates/**/*.{html,py,js}",
+    "../pub/static/js/**/*.js",
   ],
   media: false,
 };
