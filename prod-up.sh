8b169a816a5e4702e1e1a65887f1e4b9084ebfae
harlin <harlin@localhost>
2025-09-10 06:00:03 -0600

    Issue #34 - Add solution for non-stopping ollama container

--- prod-up.sh
+++ prod-up.sh
@@ -4,4 +4,7 @@ clear
 poetry export --with dev -f requirements.txt --output requirements.txt
 cp requirements.txt pub/requirements.txt
 docker compose -f docker-compose.prod.yml down -v
-docker compose -f docker-compose.prod.yml up --remove-orphans --build --force-recreate
+docker compose -f docker-compose.prod.yml up --remove-orphans --build --force-recreate -d
+
+# trap 'echo -e "\nEnter dc stop to stop containers."' SIGINT
+# docker compose logs -f
